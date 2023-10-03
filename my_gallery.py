Rotation matrix is now correct

my_gallery.py
@@ -55,24 +55,28 @@ def main():
 
 
     vehicle_matrix = vehicle_transform.get_matrix()
-    for lignes in vehicle_matrix:
-      print(lignes, "\n")
+    # for lignes in vehicle_matrix:
+    #   print(lignes, "\n")
     up_vector = vehicle_transform.get_up_vector()
     forward_vector = vehicle_transform.get_forward_vector()
     right_vector = vehicle_transform.get_right_vector()
 
     # print(vehicle_matrix)
-    print("Forward vector", forward_vector, "\n", "Right vector", right_vector  , "\n","Up vector", up_vector)
-    pitch = math.radians(vehicle_rotation.pitch)
-    roll = math.radians(vehicle_rotation.roll)
+    # print("Forward vector", forward_vector, "\n", "Right vector", right_vector  , "\n","Up vector", up_vector)
+    pitch = -math.radians(vehicle_rotation.pitch)
+    roll = -math.radians(vehicle_rotation.roll)
     yaw = math.radians(vehicle_rotation.yaw)  
 
-    matrice_roll = np.array([[1, 0, 0],[0, math.cos(roll), math.sin(roll)],[0, -math.sin(roll), math.cos(roll)]])
-    matrice_pitch = np.array([[math.cos(pitch), 0, -math.sin(pitch)],[0, 1, 0],[math.sin(pitch), 0, math.cos(pitch)]])
-    matrice_yaw = np.array([[math.cos(yaw), math.sin(yaw), 0],[-math.sin(yaw), math.cos(yaw), 0],[0, 0, 1]])
+    matrice_roll = np.array([[1, 0, 0],[0, math.cos(roll), -math.sin(roll)],[0, math.sin(roll), math.cos(roll)]])
+    matrice_pitch = np.array([[math.cos(pitch), 0, math.sin(pitch)],[0, 1, 0],[-math.sin(pitch), 0, math.cos(pitch)]])
+    matrice_yaw = np.array([[math.cos(yaw), -math.sin(yaw), 0],[math.sin(yaw), math.cos(yaw), 0],[0, 0, 1]])
 
-    rotation_matrix = np.dot(matrice_yaw,np.dot(matrice_pitch, matrice_roll))
-    print("Matrice de rotation : ",rotation_matrix)
+    # rotation_matrix = np.dot(matrice_yaw,np.dot(matrice_pitch, matrice_roll))
+    rotation_matrix = matrice_yaw @ (matrice_pitch @ matrice_roll)
+    # rotation_matrix = matrice_roll @ (matrice_pitch @ matrice_yaw)
+
+    # print("Matrice de rotation : ",rotation_matrix)
+
 
     # forward_velocity = forward_vector.get_velocity()
     # up_velocity = up_vector.get_velocity()